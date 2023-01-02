b7734bd2d2fbc8d8147bdc080a3fa38e63bf2472
Dylan Babcock <dylantbabcock@outlook.com>
2023-01-02 16:53:09 -0500

    Character-move

---
+++ Marine_Story/Assets/Scripts/Player_move.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // movement variables
+    public float moveSpeed = 5f;
+
+    // reference to the character's Rigidbody2D component
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        // get reference to the Rigidbody2D component
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // get input from the horizontal and vertical axes
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        // move the character
+        transform.position += new Vector3(horizontalInput, verticalInput, 0) * moveSpeed * Time.deltaTime;
+    }
+}
